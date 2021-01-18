151baaec61c49a6a900e2125ee39695b8906d917
yoshibox <meddacisohan@gmail.com>
2021-01-18 22:27:01 +0100

    install script

--- install_script.sh
+++ install_script.sh
@@ -2,11 +2,12 @@
 
 for repo in dwm slstatus st surf
 do
-	git clone https://git.suckless.org/$repo
+	[ ! -d "$repo" ] && git clone https://git.suckless.org/$repo
 done
 
 cp dwmconfig.h dwm/config.h
 cp laptopconfig.h slstatus/laptopconfig.h
+echo Don\'t forge to symlink/copy the corresponding config for slstatus
 cp desktopconfig.h slstatus/desktopconfig.h
 cp stconfig.h st/config.h
 cp surfconfig.h surf/config.h
