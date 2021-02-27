77d2ab312626f6e0997466963958428a27e6b6c6
yoshibox <meddacisohan@gmail.com>
2021-02-27 10:03:05 +0100

    Added rules to dwmconfig

--- dwmconfig.h
+++ dwmconfig.h
@@ -26,10 +26,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Pamac-manager", NULL,  NULL,       0,            1,           -1 },
+	/* class           instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",          NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",       NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Pamac-manager", NULL,       NULL,       0,            1,           -1 },
+	{ "Engrampa",      NULL,       NULL,       0,            1,           -1 },
+	{ "mpv",           NULL,       NULL,       0,            1,           -1 },
+	{ "Pavucontrol",   NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
